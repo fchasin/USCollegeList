a6a682cde85d1d9ac177585e0d9ca527e898ad94
Fred Chasin <fchasin52@gmail.com>
2019-04-05 15:06:54 +0100

    push again

--- components/School.js
+++ components/School.js
@@ -1,11 +1,18 @@
 export default ({ school }) => {
   return (
-    <div>
+    <div className="school">
       <div className="header" />
       <h2>School name: {school['school.name']}</h2>
       <h4>School zip: {school['school.zip']}</h4>
       <h4>School size: {school['2015.student.size']}</h4>
-      <h4>School city: {school['school.city']}</h4>
+      <h4>School city: {school['school.city']}</h4>{' '}
+      <style jsx>
+        {`
+          .school {
+            font-family: 'Lato', sans-serif;
+          }
+        `}
+      </style>
     </div>
   );
 };
